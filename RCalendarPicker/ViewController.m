3b5963f060722bc2b1d9ac3e26a2d098bc0d1b4a
roycms <roycms@qq.com>
2016-11-18 15:34:35 +0800

    添加注释

--- RCalendarPicker/ViewController.m
+++ RCalendarPicker/ViewController.m
@@ -21,8 +21,8 @@
     RCalendarPickerView *calendarPicker = [[RCalendarPickerView alloc]initWithFrame:CGRectMake(0, 0, MainScreenWidth, MainScreenHeight)];
     [self.view addSubview:calendarPicker];
     calendarPicker.isZn = YES; //开启农历
-    calendarPicker.today = [NSDate date];
-    calendarPicker.date = calendarPicker.today;
+    calendarPicker.today = [NSDate date]; //现在时间
+    calendarPicker.date = calendarPicker.today; //选择时间
     calendarPicker.complete = ^(NSInteger day, NSInteger month, NSInteger year, NSDate *date){
         NSLog(@"%d-%d-%d", (int)year,(int)month,(int)day);
     };
